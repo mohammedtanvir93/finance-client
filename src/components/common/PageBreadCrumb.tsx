2bebca0f4d4e70d46f2bca029c1e5cfa649ddaea
nhtanvir22 <tanvir.eatl@gmail.com>
2025-05-08 01:50:29 +0600

    Temporary table added

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
+interface PageLink {
+  title: string;
+  url: string;
+}
 
 interface BreadcrumbProps {
   pageTitle: string;
+  pageLinks?: PageLink[];
 }
 
-const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
+const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle, pageLinks }) => {
   return (
     <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
       <h2
@@ -22,26 +29,24 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
               href="/"
             >
               Home
-              <svg
-                className="stroke-current"
-                width="17"
-                height="16"
-                viewBox="0 0 17 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366"
-                  stroke=""
-                  strokeWidth="1.2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+              <ArrowRightIcon className="text-gray-500" width={20} height={20}/>
             </Link>
           </li>
-          <li className="text-sm text-gray-800 dark:text-white/90">
-            {pageTitle}
+          {
+            pageLinks?.map(pageLink => (
+              <li key={ 'breadcrumb-' + pageLink.title }>
+                <Link
+                  className="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
+                  href={ pageLink.url }
+                >
+                  { pageLink.title }
+                  <ArrowRightIcon className="text-gray-500" width={18} height={20}/>
+                </Link>
+              </li>
+            ))
+          }
+          <li className="mt-[2px] text-sm text-gray-800 dark:text-white/90">
+            <span>{pageTitle}</span>
           </li>
         </ol>
       </nav>
