0cad45ef39a25203829d64f63994b8b225e39388
nhtanvir22 <tanvir.eatl@gmail.com>
2025-05-23 22:20:27 +0600

    change password and sign ui fixed

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,12 +1,12 @@
 import { encrypt, decrypt } from '@/utils/encryption';
 
-export const saveLS = (key: string, data: string) => {
+export const store = (key: string, data: string) => {
     if (typeof window !== 'undefined') {
         localStorage.setItem(key, encrypt(data));
     }
 };
 
-export const getLS = (key: string) => {
+export const retrieve = (key: string) => {
     if (typeof window !== 'undefined') {
         const encrypted = localStorage.getItem(key);
 
