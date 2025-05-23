98b8922acde36145fd4505db47d33c91143771ce
nhtanvir22 <tanvir.eatl@gmail.com>
2025-05-24 00:53:20 +0600

    logout integrated

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,5 +1,10 @@
 import { encrypt, decrypt } from '@/utils/encryption';
 
+export const remove = (key: string) => {
+    if (retrieve(key))
+        localStorage.removeItem(key);
+};
+
 export const store = (key: string, data: string) => {
     if (typeof window !== 'undefined') {
         localStorage.setItem(key, encrypt(data));
